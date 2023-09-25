7e857b7a51b0f25f27f59128d2d2f36b76371ec3
Milos Ilic <milosilic89@outlook.com>
2023-09-25 23:12:23 +0200

    Created graphql schema and set-up server

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log('Hello World');
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require('graphql');
+
+// Define a simple GraphQL schema
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'Query',
+    fields: {
+      hello: {
+        type: GraphQLString,
+        resolve: () => 'Hello, World!',
+      },
+    },
+  }),
+});
+
+const app = express();
+
+// Set up a route for GraphQL using express-graphql middleware
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: true, // Enable GraphiQL for easy testing in browser
+  })
+);
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}/graphql`);
+});
