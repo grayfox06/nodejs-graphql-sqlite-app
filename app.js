ad0c06d00617f48b72799aa53e50aedefc396a64
Milos Ilic <milosilic89@outlook.com>
2023-09-25 23:18:13 +0200

    Added sqlite

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
-const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require('graphql');
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLList,
+} = require('graphql');
+const sqlite3 = require('sqlite3').verbose();
 
-// Define a simple GraphQL schema
+// Create and connect to an SQLite database
+const db = new sqlite3.Database('mydatabase.db');
+
+// Create a "posts" table in the database
+db.serialize(() => {
+  db.run(
+    'CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, content TEXT)'
+  );
+});
+
+// Define a GraphQL type for posts
+const PostType = new GraphQLObjectType({
+  name: 'Post',
+  fields: {
+    id: { type: GraphQLString },
+    title: { type: GraphQLString },
+    content: { type: GraphQLString },
+  },
+});
+
+// Define a GraphQL schema
 const schema = new GraphQLSchema({
   query: new GraphQLObjectType({
     name: 'Query',
@@ -10,6 +36,21 @@ const schema = new GraphQLSchema({
       hello: {
         type: GraphQLString,
         resolve: () => 'Hello, World!',
+      },
+      posts: {
+        type: new GraphQLList(PostType),
+        resolve: (_, args, context) => {
+          return new Promise((resolve, reject) => {
+            // Retrieve all posts from the database
+            db.all('SELECT * FROM posts', (err, rows) => {
+              if (err) {
+                reject(err);
+              } else {
+                resolve(rows);
+              }
+            });
+          });
+        },
       },
     },
   }),
@@ -22,7 +63,7 @@ app.use(
   '/graphql',
   graphqlHTTP({
     schema,
-    graphiql: true, // Enable GraphiQL for easy testing in browser
+    graphiql: true, // Enable GraphiQL for easy testing in your browser
   })
 );
 
